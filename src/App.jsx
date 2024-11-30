43028c834c316067851828ae2c1e569894889c61
ShounBiju <shounbijumcpe@gmail.com>
2024-11-30 23:47:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <Button className="bg-emerald-600">Check</Button>
+    </>
+  )
+}
+
+export default App
